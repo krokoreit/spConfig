99630920f29613e71e165376869510dde3e1c837
krokoreit <krokoreit@gmail.com>
2024-06-20 18:24:53 +0200

    aded example files

---
+++ examples/startCounter.cpp
@@ -0,0 +1,61 @@
+/**
+ * example code for spConfig library
+ * 
+ * 
+ */
+
+#include <filesystem>
+
+#include <spConfig.h>
+
+spConfig config;
+
+
+/**
+ * @brief our main function
+ * 
+ */
+int main(int argc, char *argv[])
+{
+  std::string a = argv[0];
+  printf("running %s\n", a.substr(a.rfind(std::filesystem::path::preferred_separator) + 1).c_str());
+  // ========================================================
+
+  uint32_t startCounterTotal = 0;
+  uint32_t startCounterToday = 0;
+
+  // config
+  config.setAutosave(true);
+  config.read();
+
+  time_t now = time(nullptr);
+  tm* local_tm = localtime(&now);
+
+  char todayString[7];
+  sprintf(todayString, "%4d%02d%02d", local_tm->tm_year + 1900, local_tm->tm_mon + 1, local_tm->tm_mday);
+
+  printf("todayString is %s\n", todayString);
+
+  // get config values
+  if (strcmp(todayString, config.getCStr("system", "todayString", "none")) == 0)
+  {
+    startCounterToday = config.getInt32("system", "startCounterToday", startCounterToday);
+    startCounterToday++;
+  }
+  else
+  {
+    config.setValue("system", "todayString", todayString);
+    startCounterToday = 0;
+  }
+  config.setValue("system", "startCounterToday", startCounterToday);
+  printf("Start Counter: %i\n", startCounterToday);
+  
+  startCounterTotal = config.getInt32("system", "startCounterTotal", startCounterTotal);
+  startCounterTotal++;
+  config.setValue("system", "startCounterTotal", startCounterTotal);
+  
+
+  // ========================================================
+  printf("done\n");
+  return 0;
+}
---
+++ examples/useDefaults.cpp
@@ -0,0 +1,51 @@
+/**
+ * example code for spConfig library
+ * 
+ * 
+ */
+
+
+/* 
+#include <stdio.h>
+#include <string>
+ */
+
+#include <filesystem>
+
+#include <spConfig.h>
+
+spConfig config;
+
+
+/**
+ * @brief our main function
+ * 
+ */
+int main(int argc, char *argv[])
+{
+  std::string a = argv[0];
+  printf("running %s\n", a.substr(a.rfind(std::filesystem::path::preferred_separator) + 1).c_str());
+  // ========================================================
+
+  // config
+  config.setConfigFilename("application");
+  config.setConfigDefaultFilename("default");
+  config.setConfigFileExtension("conf");
+  config.read();
+
+
+  printf("configuration values read:\n");
+  int32_t maybeInt32 = config.getInt32("maybe-overridden", "maybeInt32", 1111);
+  printf("   maybeInt32: %i\n", maybeInt32);
+  std::string maybeString = config.getString("maybe-overridden", "maybeString", "no maybeString found");
+  printf("   maybeString: %s\n", maybeString.c_str());
+  int32_t stableInt32 = config.getInt32("not-overridden", "stableInt32", 2222);
+  printf("   stableInt32: %i\n", stableInt32);
+  std::string stableString = config.getString("not-overridden", "stableString", "no stableString found");
+  printf("   stableString: %s\n", stableString.c_str());
+  printf("\nrename 'application.conf' and then 'default.conf' and see how the output changes.\n...\n");
+
+  // ========================================================
+  printf("done\n");
+  return 0;
+}
